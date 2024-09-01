fc37b30170e14a6fa9e579d52d9b70250f2ab88a
Ishara012 <isharajayarathna2001@gmail.com>
2024-09-01 23:34:51 +0530

    Items

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers';
 
 const Shop = () => {
   return (
-    <div style={{backgroundColor:'red'}}>
-      <h1>Testing title</h1>
+    <div>
+      <Hero/>
+      <Popular/>
+      <Offers/>
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
+
+ 
+
