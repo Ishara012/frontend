72f07c7c3fe5aea3db5741142ef447ea113c8390
Shehara Dewanagala <sahokavishka@gmail.com>
2024-09-04 22:49:49 +0530

    bug fixeed
    breadcrum.jsx -> line 9'
    Product.jsx -> line 19

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -3,20 +3,18 @@ import './Breadcrum.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 
 const Breadcrums = (props) => {
-    const{product} = props;
-//   return (
-//     <div className='breadcrum'>
-//       HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" />{product.name}
-//     </div>
-//   )
-// }
+
+  // error was here , call the exact prop name which is send from when this fucnction called
+    // const{product} = props;
+    const{Product} = props;
+
 
 
   return (
     <div className='breadcrum'>
       HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> 
-      {product?.category } <img src={arrow_icon} alt="" /> 
-      {product?.name}
+      {Product?.category } <img src={arrow_icon} alt="" /> 
+      {Product?.name}
     </div>
   );
 };
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
 import Breadcrums from '../Components/Breadcrums/Breadcrum';
@@ -10,10 +10,15 @@ const Product = () => {
   const{productId} = useParams();
   const Product = all_product.find((e)=>e.id === Number(productId))
 
+
+
   return (
     <div>
       <Breadcrums Product={Product}/>
        
+       {/* added test images for image view and product name */}
+       <img src={Product.image} alt=''/>
+       <div>{Product.name}</div>
       
     </div>
   )
